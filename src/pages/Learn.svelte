63ad8461aa74e54f0e7207abbf6d04120b684f2d
ogty <mofuiti1803@gmail.com>
2022-01-21 23:40:40 +0900

    update: learn page

--- src/pages/Learn.svelte
+++ src/pages/Learn.svelte
@@ -1,3 +1,8 @@
+<script>
+    import Image from "svelte-image";
+    import { Link } from "svelte-routing";
+</script>
+
 <h1 class="text-6xl text-center my-28 font-bold text-slate-900">What's requirements.txt?</h1>
 <div class="text-center text-2xl px-auto mx-auto max-w-6xl">
     <p class="my-3">
@@ -115,3 +120,58 @@
         Now that you know about <span class="font-bold">requirements.txt</span>, let's adapt it to your project!
     </p>
 </div>
+
+<div class="flex items-center justify-center text-black mt-16">
+    <div class="flex mx-1">
+        <a href="https://github.com/ogty/requirements.txt-generator/releases/download/v1.0.0/requirementstxt_generator_for_windows.zip" class="border border-black rounded p-2 hover:bg-zinc-100 flex">
+            <span class="ml-3 mt-2 mr-2">
+                <i class="fab fa-windows md:text-3xl"></i>
+            </span>
+            <span class="mr-3">
+                <span class="block text-sm">Download</span>
+                <span class="font-semibold">For Windows</span>
+            </span>
+        </a>
+    </div>
+    <div class="flex mx-1">
+        <a href="https://github.com/ogty/requirements.txt-generator/releases/download/v1.0.0/requirementstxt_generator_for_mac.zip" class="border border-black rounded p-2 hover:bg-zinc-100 flex min-w-300">
+            <span class="ml-5 mt-2 mr-2">
+                <i class="fab fa-apple md:text-3xl"></i>
+            </span>
+            <span class="mr-5">
+                <span class="block text-sm">Download</span>
+                <span class="font-semibold">For MacOS</span>
+            </span>
+        </a>    
+    </div>
+    <div class="flex mx-1">
+        <a href="https://github.com/ogty/requirements.txt-generator/" class="border border-black rounded p-2 hover:bg-zinc-100 flex">
+            <span class="ml-5 mt-2 mr-2">
+                <i class="fab fa-linux md:text-3xl"></i>
+            </span>
+            <span class="mr-6">
+                <span class="block text-sm">Download</span>
+                <span class="font-semibold">For Linux</span>
+            </span>
+        </a>        
+    </div>
+</div>
+<div class="text-center mt-3">
+    <span>or</span>
+</div>
+
+<div class="mt-3">
+    <Link to="generate">
+        <a href="/" class="mx-auto px-auto max-w-fit border border-black rounded p-2 hover:bg-zinc-100 flex">
+            <span class="ml-5 mt-2 mr-2">
+                <i class="fab fa-chrome md:text-3xl"></i>
+            </span>
+            <span class="mr-6">
+                <span class="block text-sm">Launch</span>
+                <span class="font-semibold">In Browser</span>
+            </span>
+        </a>
+    </Link>
+</div>
+
+<div class="h-56"></div>
