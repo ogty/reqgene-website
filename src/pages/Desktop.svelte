5ca8ab6492c5d1c5c05197a9d8b6e6883b8cc043
ogty <mofuiti1803@gmail.com>
2022-01-22 21:15:20 +0900

    update

--- src/pages/Desktop.svelte
+++ src/pages/Desktop.svelte
@@ -6,7 +6,7 @@
     let isPositionLanguage = false;
     window.addEventListener('DOMContentLoaded', function() {
         window.addEventListener('scroll', function() {
-            if (window.scrollY >= 900) {
+            if (window.scrollY >= 800) {
                 isPositionLanguage = true
             } else {
                 isPositionLanguage = false
@@ -15,10 +15,10 @@
     });
 </script>
 
-<div class="text-center mt-20 mb-8">
+<div class="text-center mt-28 mb-8">
     <div class="w-xl">
         <h1 class="text-8xl font-bold text-slate-900">requirements.txt Generator<br>Desktop</h1>
-        <p class="text-2xl mt-6">
+        <p class="text-2xl my-12">
             It just creates a single file, but it's fascinating.
         </p>
     </div>
@@ -60,13 +60,11 @@
     </div>
 </div>
 
-<div class="mt-16">
-    <div class="mx-auto px-auto" style="width: 900px;">
-        <Image src="images/sample.gif"/>
-    </div>
+<div class="mt-16 mb-28 mx-auto px-auto shadow-2xl" style="width: 75%;">
+    <img src="images/demo.gif" alt="">
 </div>
 
-<div class="text-center mb-6 mt-12">
+<div class="text-center mb-8">
     <div class="w-xl">
         <h1 class="text-8xl font-bold text-slate-900">Multiple language <br>support</h1>
         <p class="text-2xl mt-6">
@@ -75,65 +73,81 @@
     </div>
 </div>
 
-<div class="flex justify-between max-w-7xl px-auto mx-auto mt-14">
+<div class="flex justify-between max-w-5xl px-auto mx-auto">
     {#if isPositionLanguage}
-    <div class="mr-28" style="width: 170px;" transition:fly="{{delay: 500, duration: 800, x: -500}}">
+    <div style="width: 150px;" transition:fly="{{delay: 500, duration: 800, x: -500}}">
         <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" />
     </div>
-    <div class="mr-28" style="width: 170px;" transition:fly="{{delay: 500, duration: 800, y: 500}}">
+    <div style="width: 150px;" transition:fly="{{delay: 500, duration: 800, y: 500}}">
         <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/julia/julia-original.svg" />
     </div>
-    <div class="mr-28" style="width: 170px;" transition:fly="{{delay: 500, duration: 800, y: -500}}">
+    <div style="width: 150px;" transition:fly="{{delay: 500, duration: 800, y: -500}}">
         <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/go/go-original.svg" />
     </div>
-    <div class="" style="width: 170px;" transition:fly="{{delay: 500, duration: 800, x: 500}}">
+    <div class="" style="width: 150px;" transition:fly="{{delay: 500, duration: 800, x: 500}}">
         <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/jupyter/jupyter-original.svg" />
     </div>
     {/if}
 </div>
 
-<div class="px-auto mx-auto flex mt-80 border-2 rounded-3xl mix-w-fit text-slate-900 mx-40">
-    <div class="border-r-2 p-20">
-        <i class="fas fa-file-alt md:text-8xl my-12"></i>
-        <h1 class="text-5xl font-bold text-slate-900">Know the requirements.txt</h1>
-        <p class="mt-5 text-xl max-w-2xl">
-            What exactly is requirements.txt that most people who use Python are familiar with?
-        </p>
-        <Link to="learn">
-            <div class="mt-3 mb-5">
-                <span class="flex text-2xl font-bold">
-                    Learn more
-                    <span class="mt-1">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="none">
-                            <path stroke="#2962FF" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 22l5.5-5.5L13 11"></path>
-                        </svg>
-                    </span>
-                </span>                
-            </div>
-        </Link>
+<div class="gird-cols-2 px-auto mx-auto flex mt-48 border-2 rounded-3xl mix-w-fit text-slate-900 mx-48">
+    <div class="border-r-2 pb-8 px-20 col-start-1 col-end-2 block">
+        <div class="max-h-fit">
+            <i class="fas fa-file-alt md:text-8xl my-12"></i>
+        </div>
+        <div class="h-24">
+            <h1 class="text-5xl font-bold text-slate-900">Know the requirements.txt</h1>
+        </div>
+        <div>
+            <p class="mt-5 text-xl max-w-2xl">
+                What exactly is requirements.txt that most people who use Python are familiar with?
+            </p>
+        </div>
+        <div>
+            <Link to="learn">
+                <div class="mt-3 mb-5">
+                    <span class="flex text-2xl font-bold">
+                        Learn more
+                        <span class="mt-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="none">
+                                <path stroke="#2962FF" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 22l5.5-5.5L13 11"></path>
+                            </svg>
+                        </span>
+                    </span>                
+                </div>
+            </Link>
+        </div>
     </div>
-    <div class="p-20">
-        <i class="fas fa-eye md:text-8xl my-12"></i>
-        <h1 class="text-5xl font-bold text-slate-900">Explore our features</h1>
-        <p class="mt-5 text-xl max-w-2xl">
-            Learn more about useful features, including a detailed view of your folders.
-        </p>
-        <Link to="features">
-            <div class="mt-3 mb-5">
-                <span class="flex text-2xl font-bold">
-                    Explore features
-                    <span class="mt-1 fill-black">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="none">
-                            <path stroke="#2962FF" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 22l5.5-5.5L13 11"></path>
-                        </svg>
-                    </span>
-                </span>                
-            </div>
-        </Link>
+    <div class="px-20 pb-8 col-start-2 col-end-3 block">
+        <div class="max-h-fit">
+            <i class="fas fa-eye md:text-8xl my-12"></i>
+        </div>
+        <div class="h-24">
+            <h1 class="text-5xl font-bold text-slate-900">Explore our features</h1>
+        </div>
+        <div>
+            <p class="mt-5 text-xl max-w-2xl">
+                Learn more about useful features, including a detailed view of your folders.
+            </p>
+        </div>
+        <div>
+            <Link to="features">
+                <div class="mt-3 mb-5">
+                    <span class="flex text-2xl font-bold">
+                        Explore features
+                        <span class="mt-1 fill-black">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="none">
+                                <path stroke="#2962FF" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 22l5.5-5.5L13 11"></path>
+                            </svg>
+                        </span>
+                    </span>                
+                </div>
+            </Link>
+        </div>
     </div>
 </div>
 
-<div class="text-center mt-80 mb-8">
+<div class="text-center mt-48 mb-8">
     <div class="w-xl">
         <h1 class="text-8xl font-bold text-slate-900">No need to choose,<br> use everything</h1>
         <p class="text-2xl mt-6">
