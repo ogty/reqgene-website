a3e61a0af239c0553fb9c2eb45e4144833f33ce2
ogty <mofuiti1803@gmail.com>
2022-01-26 15:50:37 +0900

    update

--- src/pages/_Generate.svelte
+++ src/pages/_Generate.svelte
@@ -49,33 +49,44 @@
         complete = complete.filter(i => del_modules.indexOf(i) == -1)
         return complete
     }
-    function extractModules(f) {
-        let reader = new FileReader();
-        reader.onload = function() {
-            return function(e){
-                let result = module_extractor(e.target.result, "python")
-                for (let i = 0; i < result.length; i++) {
-                    jQuery("#select_modules").append(
-                        "<label for='module" + String(i) + "'>" + 
-                        "<input type='checkbox' value='" + result[i] + 
-                        "' id='module" + String(i) + 
-                        "' checked>" + result[i] + 
-                        "</label>")
-                }
-                jQuery("#download").append("<div class='hover:cursor-pointer float-right my-3 text-center'><span class='bg-slate-900 px-3 py-2 rounded-md text-white font-bold text-xl'><a download='requirements.txt' href='data:," + result.join("\n") + "'>Download</a></span><div>")
-            }
-        }(f)
-        reader.readAsText(f);
-    }
-
+    
     let files;
 	$: if (files) {
 		for (const file of files) {
             extractModules(file)
 		}
 	}
-</script>
+    function reset() {
+        jQuery("#select_modules").empty()
+        jQuery("#confirm").empty()
+        files = [];
+    }
+    function confirmed() {
+        const tmp = document.getElementsByName("module")
+        let result = []
 
+        for (let i = 0; i < tmp.length; i++) {
+            if (tmp[i].checked) {
+                result.push(tmp[i].value)
+            }
+        }
+        jQuery("#download").addClass(" border-t-2 text-slate-900")
+        jQuery("#download").append("<a download='requirements.txt' href='data:," + result.join("\n") + "'><span class='text-slate-900 md:text-7xl'><i class='fas fa-file-download'></i></span></a>")
+    }
+    function extractModules(f) {
+        let reader = new FileReader();
+        reader.onload = function() {
+            return function(e){
+                let result = module_extractor(e.target.result, "python")
+                for (let i = 0; i < result.length; i++) {
+                    jQuery("#select_modules").append("<label for='module" + String(i) + "'>" + "<input type='checkbox' value='" + result[i] + "' name='module' id='module" + String(i) + "' checked><span class='ml-2 font-bold'>" + result[i] + "</span></label>")
+                }
+                jQuery("#confirm").append("<label for='ok' class='text-white cursor-pointer py-2 px-3 bg-slate-900 font-bold float-right rounded-md'>OK</label>")
+            }
+        }(f)
+        reader.readAsText(f);
+    }
+</script>
 
 <div id="message_box">&nbsp;</div>
 <div class="px-48 h-screen">
@@ -92,7 +103,8 @@
                 </div>
             </div>
             <div class="mt-8">
-                <span class="font-bold text-xl">Selected</span>
+                <span class="font-bold text-xl mr-1">Selected</span>
+                <span class="text-slate-900 hover:cursor-pointer" on:click={reset}><i class="fas fa-redo"></i></span>
                 <div class="border-2 border-slate-900 text-center block rounded-md my-2">
                     {#if files}
                         {#each Array.from(files) as file}
@@ -107,22 +119,16 @@
                     {/if}
                 </div>
             </div>
-            <div class="mt-8">
-                <div class="flex border-2 border-slate-900 rounded-md text-center text-slate-900">
-                    <span class="w-1/2 py-2 border-r-2 border-slate-900 px-8 hover:bg-slate-900 hover:text-white">Reset</span>
-                    <span class="w-1/2 py-2 px-8 hover:bg-slate-900 hover:text-white">Generate</span>
-                </div>
-            </div>
             <div class="mt-8" id="select_modules">
                 
             </div>
-            <div id="download">
-    
+            <div class="mt-8" id="confirm">
+                <button on:click={confirmed} id="ok" class='hidden hover:cursor-pointer'></button>
             </div>
         </div>
         <div class="w-1/2 border-l-2 border-dotted border-slate-900 p-10 mx-auto px-auto">
             <div class="p-10 border-2 border-dotted border-slate-900 text-center rounded-xl block">
-                <div class="mt-48">
+                <div class="mt-24">
                     <span class="md:text-8xl text-slate-900"><i class="fas fa-file-upload"></i></span>
                 </div>
                 <div class="mt-3">
@@ -131,7 +137,7 @@
                 <div>
                     <span class="text-slate-900 text-xl">OR</span>
                 </div>
-                <div class="mt-3 mb-48 hover:cursor-pointer">
+                <div class="mt-3 mb-24 hover:cursor-pointer">
                     <span class="hidden">
                         <input id="xxx" type="file" accept=".py,.julia,.go,.ipynb" bind:files multiple>
                     </span>
@@ -139,5 +145,8 @@
                 </div>
             </div>
         </div>
+    </div>
+    <div class="mx-auto px-auto text-center mt-16" id="download">
+
     </div>
 </div>
